3a39a4895d236b64f7a9163336fc607d7bd51322
Libo Li <liboli.lilibo@gmail.com>
2022-09-20 20:38:38 -0700

    added carousel components

---
+++ src/components/Carousel/CarouseIComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="carousel"></div>
+</template>
+
+<script>
+export default {
+  // props are external parameters passed into hte component https://vuejs.org/guide/components/props.html
+  props: {},
+  // components registers other components to be used in this component
+  components: {},
+  // data is the "state" for this component. It is always a function that returns an object
+  data: () => ({
+    currentSlide: 0,
+  }),
+  methods: {},
+};
+</script>
+
+<style></style>
---
+++ src/components/Carousel/CarouselItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="carousel-item" v-show="currentSlide === index">
+    <img :src="slide" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["index", "slide"],
+};
+</script>
