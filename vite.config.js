62c1b2bc0eb9d01f3db7538ea4b33b13a31b5514
Pyline <pylinet@gmail.com>
2022-09-21 20:47:45 -0700

    set publicPath config to root

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/personalwebsite/" : "/docs/",
+  publicPath: process.env.NODE_ENV === "production" ? "/personalwebsite/" : "/",
   plugins: [vue()],
   build: {
     outDir: 'docs'
