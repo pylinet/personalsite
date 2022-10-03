daedd58e5241572d37c27ef0162606c0ac87ba67
Pyline <pylinet@gmail.com>
2022-10-03 16:03:55 -0700

    Changed public url from base url to public.

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: baseUrl,
-  publicDir: baseUrl,
+  // public folder by default '<root>/public/'
+  publicDir: 'public',
   plugins: [vue()],
   build: {
     outDir: "docs",
