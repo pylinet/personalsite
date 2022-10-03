4987382ddbd6e8468b2403d43f5b7332ead57e09
Pyline <pylinet@gmail.com>
2022-10-03 10:17:05 -0700

    Installed vue gtag for GA4.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from "vue";
-
 import router from "@/router";
 import App from "./App.vue";
+import VueGtag from "vue-gtag";
 
 import "./assets/base.css";
 import "./assets/mediaqueries.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+    .use(router)
+    .use(VueGtag, {
+        config: {
+            id: "G-D36PFVX29L",
+        }
+    }, router)
+    .mount("#app");
